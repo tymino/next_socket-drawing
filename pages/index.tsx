3a903737a83c12717beb3508a7d3aa6b9e28a610
tymino <d_team-lex@bk.ru>
2022-03-03 16:16:18 +0300

    save

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,8 +13,6 @@ const Home: NextPage = () => {
   const refCanvas = useRef() as React.MutableRefObject<HTMLCanvasElement>;
 
   const [brush, setBrush] = useState({
-    x: 0,
-    y: 0,
     color: '#fff',
     size: 10,
     down: false,
@@ -27,26 +25,26 @@ const Home: NextPage = () => {
     points: [],
   };
 
-  const draw = () => {
+  const draw = (strokeData: any) => {
     if (!ctx) return;
     ctx.lineCap = 'round';
 
-    for (let i = 0; i < strokes.length; i++) {
-      const s = strokes[i];
-      ctx.strokeStyle = s.color;
-      ctx.lineWidth = s.size;
+    strokeData.forEach((stroke: any) => {
+      ctx.strokeStyle = stroke.color;
+      ctx.lineWidth = stroke.size;
+
       ctx.beginPath();
-      ctx.moveTo(s.points[0].x, s.points[0].y);
-      for (var j = 0; j < s.points.length; j++) {
-        var p = s.points[j];
+      ctx.moveTo(stroke.points[0].x, stroke.points[0].y);
+      for (var j = 0; j < stroke.points.length; j++) {
+        var p = stroke.points[j];
         ctx.lineTo(p.x, p.y);
       }
       ctx.stroke();
-    }
+    });
   };
 
   const sendPoints = async (data: any) => {
-    socket.emit(NameSocket.Draws, { x: data.x, y: data.y });
+    socket.emit(NameSocket.Draws, data);
   };
 
   const handleMouseEvent = (e: any) => {
@@ -55,31 +53,26 @@ const Home: NextPage = () => {
 
     const data = { x: localX, y: localY };
 
-    currentStroke.points.push(data);
-
-    sendPoints(data);
-
-    draw();
-  };
-
-  const handleMouseDown = (e: any) => {
-    brush.down = true;
-
-    currentStroke = {
+    const currentStroke: any = {
       color: '#fff',
       size: 10,
       points: [],
     };
+    currentStroke.points.push(data);
 
-    strokes.push(currentStroke);
+    setStrokes([...strokes, currentStroke]);
 
+    draw([...strokes, currentStroke]);
+  };
+
+  const handleMouseDown = (e: any) => {
+    brush.down = true;
     handleMouseEvent(e);
   };
 
   const handleMouseUp = (e: any) => {
     brush.down = false;
-    currentStroke = null;
-    // handleMouseEvent(e);
+    sendPoints(strokes);
   };
   const handleMouseMove = (e: any) => {
     if (brush.down) handleMouseEvent(e);
@@ -98,23 +91,25 @@ const Home: NextPage = () => {
 
   useEffect(() => {
     socket.on(NameSocket.Сoloring, (data) => {
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      currentStroke = {
-        color: '#fff',
-        size: 10,
-        points: [],
-      };
+      setStrokes(data);
 
-      strokes.push(currentStroke);
+      if (!ctx) return;
+      ctx.lineCap = 'round';
 
-      currentStroke.points.push({
-        x: data.x,
-        y: data.y,
+      data.forEach((stroke: any) => {
+        ctx.strokeStyle = stroke.color;
+        ctx.lineWidth = stroke.size;
+
+        ctx.beginPath();
+        ctx.moveTo(stroke.points[0].x, stroke.points[0].y);
+        for (var j = 0; j < stroke.points.length; j++) {
+          var p = stroke.points[j];
+          ctx.lineTo(p.x, p.y);
+        }
+        ctx.stroke();
       });
-
-      draw();
     });
-  }, [currentStroke.points, draw, socket]);
+  }, [socket, ctx]);
 
   return (
     <div className={styles.container}>
