5933c16832fdd345396c37183b0a6534cd45aa46
tymino <d_team-lex@bk.ru>
2022-03-10 16:20:41 +0300

    styles

--- components/Range/Range.tsx
+++ components/Range/Range.tsx
@@ -6,22 +6,23 @@ import { IRangeProps } from '../../types';
 const Range: FC<IRangeProps> = ({ name, minValue, maxValue, size, handleChangeSize }) => {
   return (
     <div className={st.container}>
-      <label className={st.label} htmlFor="size">
-        {name}
-      </label>
       <div className={st.wrapper}>
-        <input
-          className={st.input}
-          type="range"
-          name="size"
-          step={1}
-          min={minValue}
-          max={maxValue}
-          value={size}
-          onChange={handleChangeSize}
-        />
+        <label className={st.label} htmlFor="size">
+          {name}
+        </label>
         <div className={st.value}>{size}</div>
       </div>
+
+      <input
+        className={st.input}
+        type="range"
+        name="size"
+        step={1}
+        min={minValue}
+        max={maxValue}
+        value={size}
+        onChange={handleChangeSize}
+      />
     </div>
   );
 };
